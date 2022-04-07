3799599893bd1aa807409d798072e3aae7e9d1e6
Bukkiee20 <adeyooyebukola22@gmail.com>
2022-04-07 10:55:30 +0100

    Updated color flipper project

--- color-flipper.js
+++ color-flipper.js
@@ -1,8 +1,10 @@
+color=document.getElementById("values");
 function rgb(){
     var x= Math.floor(Math.random() * 256);
     var y= Math.floor(Math.random() * 256);
     var z= Math.floor(Math.random() * 256);
 
     var rgbColor= "rgb("+ x + ',' + y + ','+ z + ")";
+    color.textContent= "Background Color: " + rgbColor;
     document.body.style.backgroundColor= rgbColor;
 }
